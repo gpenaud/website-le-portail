5e834ef709a5a3408bca9eab854aeb4494c0ea13
Administrator <admin@example.com>
2022-05-31 09:04:49 +0200

    Add two scripts, one to retrieve sources from FTP, another to reduce images size

---
+++ scripts/get-sources-from-ftp.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# a (relatively) good place to store your credentials (outside repo, ...)
+read ftp_credentials < ~/.lftp/leportail.credentials
+
+if [ -z "$ftp_credentials" ]; then
+  echo "Could NOT read credentials. Exiting."
+  exit 2
+fi
+
+lftp -c " \
+  set \
+    ssl:verify-certificate no; \
+    ftp:list-options -a; \
+  open $ftp_credentials; \
+  mirror \
+      --continue \
+      --verbose \
+      --parallel=3 \
+      -x '^tmp\/cache\/' \
+    /v3/ \
+    /home/gpenaud/work/ecolieu/previous-website-le-portail/backups/src; \
+  ; \
+  close -a;"
---
+++ scripts/resize-images.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env
+
+# absolute path to image folder
+FOLDER="app"
+
+# max width
+WIDTH=640
+
+# max height
+HEIGHT=480
+
+#resize png or jpg to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.jpg' -o -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize png to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize jpg only to either height or width, keeps proportions using imagemagick
+find ${FOLDER} -iname '*.jpg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+find ${FOLDER} -iname '*.JPG' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+find ${FOLDER} -iname '*.jpeg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+find ${FOLDER} -iname '*.JPEG' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+find ${FOLDER} -iname '.*.jpg' -exec rm --force \{} \;
+find ${FOLDER} -iname '.*.JPG' -exec rm --force \{} \;
+find ${FOLDER} -iname '.*.jpeg' -exec rm --force \{} \;
+find ${FOLDER} -iname '.*.JPEG' -exec rm --force \{} \;
+
+# alternative
+#mogrify -path ${FOLDER} -resize ${WIDTH}x${HEIGHT}% *.png -verbose
